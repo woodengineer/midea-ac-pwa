bfcd6ee525e3480baa3fa4cb54e4fa95702caa40
woodengineer <37389874+woodengineer@users.noreply.github.com>
2026-09-11 15:06:41 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE = "midea-ac-pwa-v0.2.6";
+const CACHE = "midea-ac-pwa-v0.2.7";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./app.css?v=0.2.6",
-  "./app.js?v=0.2.6",
+  "./app.css?v=0.2.7",
+  "./app.js?v=0.2.7",
   "./manifest.webmanifest",
   "./icons/icon-192.png",
   "./icons/icon-512.png"
